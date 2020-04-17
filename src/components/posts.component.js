972a39bb8522d7102556cd3ef02b674251f4b8a9
Leks <leks511@bk.ru>
2020-04-17 23:22:46 +0300

    Added rendering posts on posts section

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -1,4 +1,7 @@
 import {Component} from "../core/component";
+
+import {POST_TYPES} from "../core/constants";
+
 import {apiService} from "../services/api.service";
 import {TransformService} from "../services/transform.service";
 
@@ -10,8 +13,47 @@ class PostsComponent extends Component {
   async onShow() {
     const postsData = await apiService.fetchPosts();
     const posts = TransformService.fbObjectToArray(postsData);
-    console.log(posts)
+    const html = posts.map(post => renderPosts(post)).join(``);
+
+    this.el.insertAdjacentHTML(`afterbegin`, html);
   }
+
+  onHide() {
+    this.el.innerHTML = ``;
+  }
+}
+
+function getTag(typeCode) {
+  const tag = POST_TYPES[typeCode];
+  const tagClass = typeCode === `news` ? `tag-blue` : ``
+
+  return `<li class="tag ${tagClass} tag-rounded">${tag}</li>`;
+}
+
+function renderPosts(postData) {
+  const {title, fulltext, date, type} = postData;
+
+  const tagMorkup = getTag(type);
+
+  const button = `<button type="button" class="button-round button-small button-primary">Сохранить</button>`;
+
+  return (
+    `<div class="panel">
+      <div class="panel-head">
+        <p class="panel-title">${title}</p>
+        <ul class="tags">
+          ${tagMorkup}
+        </ul>
+      </div>
+      <div class="panel-body">
+        <p class="multi-line">${fulltext}</p>
+      </div>
+      <div class="panel-footer w-panel-footer">
+        <small>${date}</small>
+        ${button}
+      </div>
+    </div>`
+  );  
 }
 
 export default PostsComponent;
